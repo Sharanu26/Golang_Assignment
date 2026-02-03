7da0387ab0c43b5427ef38f67e3db10252d124a0
Sharanaabasppa Halle <135137499+Sharanu26@users.noreply.github.com>
2026-02-03 11:02:20 +0530

    Refactor employee management system code
    Reformatted code and added task description for employee management system.

--- Assignment2.go
+++ Assignment2.go
@@ -1,3 +1,11 @@
+/* Task 2:-
+Develop a system to manage employees and their departments.
+Each employee has a name, age, and salary. Each department
+has a name, a list of employees, and a method to calculate
+the average salary of its employees. Additionally, create
+methods to add and remove employees from departments and to give a raise to an employee.*/
+
+
 package main
 
 import (
@@ -127,6 +135,7 @@ After 10% raise for Employee Id: 102: Salary is 12100
 
 
 	
+
 
 
 
